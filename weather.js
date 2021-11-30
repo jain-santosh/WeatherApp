6c1e2f9c7a93d2433d73f179085d8f4a8a620df8
Santosh Jain <48131320+jain-santosh@users.noreply.github.com>
2021-11-30 17:58:57 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    
+    document.querySelector('form').onsubmit = function() {
+        var q = document.querySelector('#location').value;
+        fetch(`http://api.weatherapi.com/v1/current.json?key=16776569ce554b44914135907212911&q=${q}`)
+        .then(response => response.json())
+        .then(data => {
+            if(data != undefined){
+                //location name
+                document.querySelector('#loc').innerHTML = data.location.name;
+                var localtime = data.location.localtime;
+                const dateTime = localtime.split(" ");
+                //time
+                document.querySelector('#time').innerHTML = dateTime[1];
+                //feels like celcius
+                document.querySelector('#feel').innerHTML = `Feels like ${data.current.feelslike_c}`;
+                //temperature celcius
+                document.querySelector('#tempc').innerHTML = `${data.current.temp_c}&#176;`;
+                //weather condition
+                document.querySelector('#cond').innerHTML = data.current.condition.text;
+            } else {
+                document.querySelector('#result').innerHTML = `Invalid Location! Try again`
+            }
+            
+        })
+
+        .catch(error => {
+            console.log('Error: ',error);
+        })
+
+        return false;
+    }
+
+    
+});
